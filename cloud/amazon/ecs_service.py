4d3fd2f34ec7c00031cfbbe16591714f4197dd60
Justin Menga <justin.menga@gmail.com>
2016-01-02 11:00:23 -0500

    Correct datetime object type in Boto fix for datetime types

--- cloud/amazon/ecs_service.py
+++ cloud/amazon/ecs_service.py
@@ -289,7 +289,7 @@ class EcsServiceManager:
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
-    if isinstance(obj, datetime):
+    if isinstance(obj, datetime.datetime):
         serial = obj.isoformat()
         return serial
     raise TypeError ("Type not serializable")
